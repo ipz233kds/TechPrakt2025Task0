83e546f9e3ab915ff5e854d941a38544cdde72da
Danylo Krukovskyi <ipz233_kds@ztu.edu.ua>
2025-01-23 00:57:49 +0200

    Додано консольний проєкт на C#

---
+++ C#/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
